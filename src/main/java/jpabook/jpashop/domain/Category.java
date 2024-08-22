0d37e5419770eab66e7e7a01984cabef7c8244e4
배석현 <bjh4966@naver.com>
2024-08-22 19:57:06 +0900

    3

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> children = new ArrayList<Category>();
+
+    @ManyToMany
+    @JoinTable(name="CATEGORY_ITEM", joinColumns = @JoinColumn(name = "CATEGORT_ID"), inverseJoinColumns = @JoinColumn(name = "ITEM_ID"))
+    private List<Item> items = new ArrayList<>();
+}
